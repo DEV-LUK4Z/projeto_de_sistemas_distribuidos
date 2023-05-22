9a1b839da50053e296ad3ec7d0b388feb1dcf5b4
DEV-LUK4Z <lucassoares1099@outlook.com>
2023-05-22 09:51:52 -0300

    Criando um novo código

--- client/client.py
+++ client/client.py
@@ -1,16 +1,34 @@
 import socket
+import pickle
 
-HOST = 'localhost'
-PORT = 55552
+HOST = '127.0.0.1'  # Endereço IP do servidor
+PORT = 1234  # Porta para conexão
 
-print(f'\nHOST: {HOST}\nPORT: {PORT}')
+# Cria o socket do cliente
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
-print("\nCONECTADO!\n")
+# Recebe a pergunta e as opções do servidor
+pergunta = client_socket.recv(1024).decode()
+opcoes = pickle.loads(client_socket.recv(1024))
 
-mensagem = input("Enviar mensagem >> ")
-client.sendall(str.encode(mensagem))
+# Exibe a pergunta e as opções para o usuário
+print(pergunta)
+for opcao in opcoes:
+    print(opcao)
 
-data = client.recv(1024)
-print(f'\nMensagem ecoada do servidor: {data.decode()}')
+# Solicita ao usuário a resposta
+resposta = int(input("Digite o número da opção correta: "))
+client_socket.send(str(resposta).encode())
+
+# Recebe o resultado do servidor
+resultado = client_socket.recv(1024).decode()
+
+# Verifica se a resposta está incorreta e exibe a mensagem correspondente
+if resultado == "Resposta incorreta!":
+    print(resultado)
+else:
+    print(resultado)
+
+# Fecha a conexão
+client_socket.close()
--- server/server.py
+++ server/server.py
@@ -1,22 +1,41 @@
 import socket
+import pickle
 
-HOST = 'localhost'
-PORT = 55552
+HOST = '127.0.0.1'  # Endereço IP do servidor
+PORT = 1234  # Porta para conexão
 
-print(f'\nHOST: {HOST}\nPORT: {PORT}')
+# Cria o socket do servidor
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(5)
-print("\nServidor aguardando conexão com o cliente")
+print('Aguardando conexão do cliente...')
 
-client, address = server.accept()
+# Aceita a conexão do cliente
+client_socket, addr = server_socket.accept()
+print('Cliente conectado:', addr)
 
-while True:
-    data = client.recv(1024).decode()
-    if not data:
-        print("\nConexão Encerrada!")
-        client.close()
-        break
-    print(f'\nMensagem do cliente: {data}')
-    client.sendall(data.encode())
+# Define a pergunta, as opções e a resposta correta
+pergunta = "Qual é a capital do Brasil?"
+opcoes = ["1 - Belém", "2 - Salvador", "3 - Brasília", "4 - Castanhal"]
+resposta_correta = 3
+
+# Envia a pergunta e as opções para o cliente
+client_socket.send(pergunta.encode())
+client_socket.send(pickle.dumps(opcoes))
+
+# Recebe a resposta do cliente
+resposta_cliente = int(client_socket.recv(1024).decode())
+
+# Verifica se a resposta está correta
+if resposta_cliente == resposta_correta:
+    resultado = "Resposta correta!"
+else:
+    resultado = "Resposta incorreta!"
+
+# Envia o resultado para o cliente
+client_socket.send(resultado.encode())
+
+# Fecha a conexão
+client_socket.close()
+server_socket.close()
