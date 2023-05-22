fec42b39f7fb4fbcaebf59b44617d573e2a1ef30
DEV-LUK4Z <lucassoares1099@outlook.com>
2023-05-22 10:02:23 -0300

    Estilização dos prints e outros

--- client/client.py
+++ client/client.py
@@ -2,11 +2,12 @@ import socket
 import pickle
 
 HOST = '127.0.0.1'  # Endereço IP do servidor
-PORT = 1234  # Porta para conexão
+PORT = 50001  # Porta para conexão
 
 # Cria o socket do cliente
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
+print("\nConectado ao servidor")
 
 # Recebe a pergunta e as opções do servidor
 pergunta = client_socket.recv(1024).decode()
@@ -18,7 +19,7 @@ for opcao in opcoes:
     print(opcao)
 
 # Solicita ao usuário a resposta
-resposta = int(input("Digite o número da opção correta: "))
+resposta = int(input("\nDigite o número da opção correta >> "))
 client_socket.send(str(resposta).encode())
 
 # Recebe o resultado do servidor
@@ -26,9 +27,9 @@ resultado = client_socket.recv(1024).decode()
 
 # Verifica se a resposta está incorreta e exibe a mensagem correspondente
 if resultado == "Resposta incorreta!":
-    print(resultado)
+    print("\n",resultado)
 else:
-    print(resultado)
+    print("\n",resultado)
 
 # Fecha a conexão
 client_socket.close()
--- server/server.py
+++ server/server.py
@@ -1,15 +1,15 @@
 import socket
 import pickle
 
-HOST = '127.0.0.1'  # Endereço IP do servidor
-PORT = 1234  # Porta para conexão
+HOST = 'localhost'  # Endereço IP do servidor
+PORT = 50001  # Porta para conexão
 
 # Cria o socket do servidor
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(1)
 
-print('Aguardando conexão do cliente...')
+print('\nAguardando conexão do cliente...')
 
 # Aceita a conexão do cliente
 client_socket, addr = server_socket.accept()
@@ -26,12 +26,13 @@ client_socket.send(pickle.dumps(opcoes))
 
 # Recebe a resposta do cliente
 resposta_cliente = int(client_socket.recv(1024).decode())
+print(f'\nResposta do Cliente: {resposta_cliente}')
 
 # Verifica se a resposta está correta
 if resposta_cliente == resposta_correta:
-    resultado = "Resposta correta!"
+    resultado = "RESPOSTA CORRETA!!"
 else:
-    resultado = "Resposta incorreta!"
+    resultado = "RESPOSTA INCORRETA!!"
 
 # Envia o resultado para o cliente
 client_socket.send(resultado.encode())
