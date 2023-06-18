4bf1f153894583370fff62461021a2d71f24e7d7
DEV-LUK4Z <lucassoares1099@outlook.com>
2023-06-18 12:34:30 -0300

    saber quantos clientes estão conectados

--- server/server.py
+++ server/server.py
@@ -5,35 +5,42 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1234
 
-def handle_client(client_socket):
-    perguntas = [
-        ("Qual empresa lançou o primeiro processador?", ["1 - IBM", "2 - AMD", "3 - Intel", "4 - Motorola"], 3),
-        ("Qual foi o primeiro sistema de correio eletrônico?", ["1 - ARPANET Mail", "2 - Gmail", "3 - Hotmail", "4 - Yahoo Mail"], 1),
-        ("Qual é o nome da linguagem de programação desenvolvida pela Microsoft?", ["1 - Python", "2 - C++", "3 - Java", "4 - C#"], 4),
-        ("Qual é o protocolo de internet utilizado para acessar páginas web?", ["1 - FTP", "2 - HTTP", "3 - TCP", "4 - IP"], 2),
-        ("Qual é o nome da tecnologia que permite a conexão de dispositivos\n à internet por meio de ondas de rádio?", ["1 - Wi-Fi", "2 - Ethernet", "3 - Bluetooth", "4 - 3G/4G/5G"], 4),
-        ("Que ano foi lançado o Macintosh?", ["1 - 1980", "2 - 1982", "3 - 1984", "4 - 1986"], 3)
-    ]
-    acertos = 0
+connected_clients = []
 
-    for pergunta, opcoes, resposta_correta in perguntas:
-        client_socket.send(pickle.dumps((pergunta, opcoes, False)))
+def handle_client(client_socket, addr):
+    try:
+        perguntas = [
+            ("Qual empresa lançou o primeiro processador?", ["1 - IBM", "2 - AMD", "3 - Intel", "4 - Motorola"], 3),
+            ("Qual foi o primeiro sistema de correio eletrônico?", ["1 - ARPANET Mail", "2 - Gmail", "3 - Hotmail", "4 - Yahoo Mail"], 1),
+            ("Qual é o nome da linguagem de programação desenvolvida pela Microsoft?", ["1 - Python", "2 - C++", "3 - Java", "4 - C#"], 4),
+            ("Qual é o protocolo de internet utilizado para acessar páginas web?", ["1 - FTP", "2 - HTTP", "3 - TCP", "4 - IP"], 2),
+            ("Qual é o nome da tecnologia que permite a conexão de dispositivos\n à internet por meio de ondas de rádio?", ["1 - Wi-Fi", "2 - Ethernet", "3 - Bluetooth", "4 - 3G/4G/5G"], 4),
+            ("Que ano foi lançado o Macintosh?", ["1 - 1980", "2 - 1982", "3 - 1984", "4 - 1986"], 3)
+        ]
+        acertos = 0
 
-        resposta_cliente = int(client_socket.recv(1024).decode())
+        for pergunta, opcoes, resposta_correta in perguntas:
+            client_socket.send(pickle.dumps((pergunta, opcoes, False)))
 
-        if resposta_cliente == resposta_correta:
-            acertos += 1
-            client_socket.send('acertou'.encode())
+            resposta_cliente = int(client_socket.recv(1024).decode())
+
+            if resposta_cliente == resposta_correta:
+                acertos += 1
+                client_socket.send('acertou'.encode())
+            else:
+                client_socket.send('errou'.encode())
+                break
+
+        if acertos == 5:
+            client_socket.send(pickle.dumps(("Você acertou todas as perguntas!", [], True)))
         else:
-            client_socket.send('errou'.encode())
-            break
-
-    if acertos == 3:
-        client_socket.send(pickle.dumps(("Você acertou todas as perguntas!", [], True)))
-    else:
-        client_socket.send(pickle.dumps(("Fim de Jogo", [], True)))
-
-    client_socket.close()
+            client_socket.send(pickle.dumps(("Fim de Jogo", [], True)))
+    except ConnectionResetError:
+        print('Cliente desconectado:', addr)
+    finally:
+        client_socket.close()
+        connected_clients.remove((client_socket, addr))
+        print('Clientes conectados:', len(connected_clients))
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,8 +52,10 @@ def start_server():
     while True:
         client_socket, addr = server_socket.accept()
         print('Cliente conectado:', addr)
+        connected_clients.append((client_socket, addr))
+        print('Clientes conectados:', len(connected_clients))
 
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
         client_thread.start()
 
 start_server()
